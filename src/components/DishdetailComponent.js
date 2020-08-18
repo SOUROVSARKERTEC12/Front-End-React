05ec2190fbd740c0c60c74efc031124913d5119f
SOUROV <sourov@gmail.com>
2020-08-18 17:19:45 +0600

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
-
-    renderDish(dish) {
+    function RenderDish({dish}){
         return (
             <div className="col-12 col-md-5 m-1">
                 <Card>
@@ -17,7 +15,7 @@ class Dishdetail extends Component {
         );
     }
 
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments != null) {
             return (
                 <div className="col-12 col-md-5 m-1">
@@ -45,13 +43,13 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        if (this.props.dish != null) {
+    const Dishdetail=(props) =>{
+        if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments dish={props.dish.comments}/>
                     </div>
                 </div>
             );
@@ -62,6 +60,6 @@ class Dishdetail extends Component {
             );
         }
     }
-}
+
 
 export default Dishdetail;
