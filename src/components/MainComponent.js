2c3bf2ff80fa1665e19540c48787b380b902cf35
SOUROV <sourov@gmail.com>
2020-08-22 13:03:56 +0600

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -52,11 +52,11 @@ class Main extends Component {
                 <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage}/>
-                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
                     <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
                     <Route exact path="/contactus" component={Contact}/>
                     <Route path="/menu/:dishId" component={DishWithId}/>
-                    <Redirect  to ="/home"/>
+                    <Redirect to ="/home"/>
                 </Switch>
                 <Footer/>
             </div>
